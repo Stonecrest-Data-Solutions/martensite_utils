c35c2ff58d9b98dc656044b0055414dc6bf5c529
Dominik Steinberg <dsteinberg@stonecrestdata.com>
2025-05-14 21:27:17 -0400

    Added docstrings to

--- martensite_utils/to_numpy.py
+++ martensite_utils/to_numpy.py
@@ -11,12 +11,25 @@ numpy_types = {
 
 
 def proto_to_numpy(proto_message: message_pb2.NDArrayProto) -> np.ndarray:
+    """
+    Converts a Protobuf message into a numpy array. The protobuf message must be the NDArrayProto in
+    martensite_utils.message_pb2.
+
+    :param proto_message: A martensite_utils.messasge_pb2.NDArrayProto protobuf message
+    :return: The numpy array that was serialized into the protobuf message.
+    """
     dtype = numpy_types[proto_message.dtype]
     array = np.frombuffer(proto_message.data, dtype=dtype).reshape(proto_message.shape)
     return array
 
 
 def raw_proto_to_numpy(data: bytes) -> np.ndarray:
+    """
+    Converts raw bytes that were serialized using the NDArrayProto into a numpy array.
+
+    :param data: The bytes that contain the protobuf message.
+    :return: The numpy array that was serialized into the protobuf message.
+    """
     proto = message_pb2.NDArrayProto()
     proto.ParseFromString(data)
 
